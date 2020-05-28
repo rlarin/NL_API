d5bd33563fa8bdaec53159b412c2a292c74d14a9
Rainer Larin-Fonseca <rlarinf@gmail.com>
2020-05-28 17:17:17 +0200

    Heroku deployment heroku config:set STATICS

--- NL_API/settings.py
+++ NL_API/settings.py
@@ -121,10 +121,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 GRAPHENE = {
     'SCHEMA': 'NL_API.schema.schema',
