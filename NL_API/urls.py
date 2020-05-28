c46ecabc14dd1661fd02128c8ca4eed7a0e0e6bb
Rainer Larin-Fonseca <rlarinf@gmail.com>
2020-05-28 21:55:37 +0200

    Static landing page included: ok

--- NL_API/urls.py
+++ NL_API/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import TemplateView
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='landing/index.html')),
+
+    # path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
