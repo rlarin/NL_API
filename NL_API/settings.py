5cfcb8165dd953ae3e40631fb004e1f47afbccdc
Rainer Larin-Fonseca <rlarinf@gmail.com>
2020-05-28 17:20:53 +0200

    Heroku deployment heroku config:set STATICS

--- NL_API/settings.py
+++ NL_API/settings.py
@@ -127,11 +127,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
-
 GRAPHENE = {
     'SCHEMA': 'NL_API.schema.schema',
     'MIDDLEWARE': [
